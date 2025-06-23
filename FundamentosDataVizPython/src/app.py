b01c36b532b9bf4d2d90b9a572ca7efe452d0f62
Santiago Puerta <puertasantiago2002@gmail.com>
2025-06-23 13:39:34 -0500

    fix: Fixed CSV path for streamlit

--- FundamentosDataVizPython/src/app.py
+++ FundamentosDataVizPython/src/app.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
 
     tab1, tab2 = st.tabs(["🔍 Visión General", "🏙️ Vista Ciudad"])
 
-    PATH: str = r"data/planeta_salud_wellbeing_dataset.csv"
+    PATH: str = "FundamentosDataVizPython/data/planeta_salud_wellbeing_dataset.csv"
     data: pd.DataFrame = pd.read_csv(PATH)
     
     with tab1:
